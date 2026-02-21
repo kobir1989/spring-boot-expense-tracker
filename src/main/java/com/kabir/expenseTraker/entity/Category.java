0e9db3da84b81fdcde18f34a4fadc9f950c97a57
Kabir Hossain <kobir.h.ritu@gmail.com>
2026-02-21 20:40:56 +0600

    feat: add category entity

--- src/main/java/com/kabir/expenseTraker/entity/Category.java
+++ src/main/java/com/kabir/expenseTraker/entity/Category.java
@@ -3,6 +3,7 @@ package com.kabir.expenseTraker.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,5 +12,12 @@ public class Category {
   @Id
   @GeneratedValue(strategy=GenerationType.IDENTITY)
   private Long id; 
+
   private String category;
+
+  public Category() {}
+  public Long getId(){ return id;}
+
+  public String getCategory(){ return category;}
+  public String setCategory (String category) {this.category = category;return category;}
 }
