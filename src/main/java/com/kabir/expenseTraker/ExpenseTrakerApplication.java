8747b7487e10be58719868db9ac7bc5d80538d91
Kabir Hossain <kobir.h.ritu@gmail.com>
2026-02-21 20:35:01 +0600

    chore: initial commit

---
+++ src/main/java/com/kabir/expenseTraker/ExpenseTrakerApplication.java
@@ -0,0 +1,13 @@
+package com.kabir.expenseTraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrakerApplication.class, args);
+	}
+
+}
